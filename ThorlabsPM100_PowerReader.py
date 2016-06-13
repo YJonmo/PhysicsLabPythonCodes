2113e78255f1e566015f00bd63433fc56d702e40
YJonmo <y.jonmo@auckland.ac.nz>
2016-06-13 14:14:33 +1200

    Update ThorlabsPM100_PowerReader.py

--- ThorlabsPM100_PowerReader.py
+++ ThorlabsPM100_PowerReader.py
@@ -41,33 +41,34 @@ def PlotSignal(TimeIndex, Power):               # This function plots the record
 def SaveData(TimeIndex, Power):                          # This function save the recorded date in the HDF5 format. You don't need to call it when using for testing.
     File_name = "Chose_a_Name_ThorlabsPM100" + str('%s' %datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d-%H-%M-%S'))+ ".hdf5"
     file = h5py.File(File_name, "w")
-    Spec_subgroup1 = file.create_group("ThorlabsPM100")
-    Spec_intensities = file.create_dataset('ThorlabsPM100/Power', data = Power)
-    Spec_wavelength = file.create_dataset('ThorlabsPM100/TimeIndex', data = TimeIndex)
+    P100_subgroup1 = file.create_group("ThorlabsPM100")
+    P100_Powers = file.create_dataset('ThorlabsPM100/Power', data = Power)
+    P100_TimeIndex = file.create_dataset('ThorlabsPM100/TimeIndex', data = TimeIndex)
     #dset.attrs["attr"] = b"Hello"
-    #Spec_subgroup1.attrs['ThorlabsPM100 Details'] = np.string_(ThorlabsPM100.getDetails())
+    #P100_subgroup1.attrs['ThorlabsPM100 Details'] = np.string_(ThorlabsPM100.getDetails())
     file.close()
 
+if __name__ == "__main__":
 
-for I in range(No_Power_Sample):
-    try:
-        Time_Label = time.time()
-        Read_Power[I] = Power_meter.readPower()
-        Read_TimeIndex[I] = int(round(time.time() * 1000))
+	for I in range(No_Power_Sample):
+	    try:
+	        Time_Label = time.time()
+	        Read_Power[I], Read_TimeIndex[I] = Power_meter.readPower()
 
-        if I < No_Power_WindowPlot:                                      # This 'if' and 'else' are used when you want to plot the read signal on the sensor power. If you want to record the signal as fast as possible then you need to commend this 'if' and 'else'.
-            Read_Power_WindowPlot[I: :-1] = Read_Power[I: :-1]
-            PlotSignal(Read_TimeIndex[I : :-1] - Read_TimeIndex[0], Read_Power_WindowPlot[I: :-1])      # This calls the function for plotting the intensities and it incures delay on reading the intensities. Comment out this line (by #) if you want to read the intensities according to the integration time.
-        else:
-            Read_Power_WindowPlot = Read_Power[ I : I - No_Power_WindowPlot : -1]
-            PlotSignal(Read_TimeIndex[I : I - No_Power_WindowPlot : -1] - Read_TimeIndex[0], Read_Power_WindowPlot)      # This calls the function for plotting the intensities and it incures delay on reading the intensities. Comment out this line (by #) if you want to read the intensities according to the integration time.
+		'''
+	        if I < No_Power_WindowPlot:                                      # This 'if' and 'else' are used when you want to plot the read signal on the sensor power. If you want to record the signal as fast as possible then you need to commend this 'if' and 'else'.
+	            Read_Power_WindowPlot[I: :-1] = Read_Power[I: :-1]
+	            PlotSignal(Read_TimeIndex[I : :-1] - Read_TimeIndex[0], Read_Power_WindowPlot[I: :-1])      # This calls the function for plotting the intensities and it incures delay on reading the intensities. Comment out this line (by #) if you want to read the intensities according to the integration time.
+	        else:
+	            Read_Power_WindowPlot = Read_Power[ I : I - No_Power_WindowPlot : -1]
+	            PlotSignal(Read_TimeIndex[I : I - No_Power_WindowPlot : -1] - Read_TimeIndex[0], Read_Power_WindowPlot)      # This calls the function for plotting the intensities and it incures delay on reading the intensities. Comment out this line (by #) if you want to read the intensities according to the integration time.
+		'''
+	        print ("Last power was read %f seconds ago" % (time.time() - Time_Label))
+	        I = I + 1
+	    except KeyboardInterrupt:
+	        break
 
-        print ("Last power was read %f seconds ago" % (time.time() - Time_Label))
-        I = I + 1
-    except KeyboardInterrupt:
-        break
 
+	SaveData(Read_TimeIndex, Read_Power)       # This calls the function to save the recorded data in the HDF5 format. You can comment it out (by #) when using this code for testing.
 
-SaveData(Read_TimeIndex, Read_Power)       # This calls the function to save the recorded data in the HDF5 format. You can comment it out (by #) when using this code for testing.
-
-#Power_meter.close()
+	#Power_meter.close()
