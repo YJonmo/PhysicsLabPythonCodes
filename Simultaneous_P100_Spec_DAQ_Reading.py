63633d3644ce89dbcc140eb2ac4427e1bff7ac2b
YJonmo <y.jonmo@auckland.ac.nz>
2016-06-13 17:39:32 +1200

    Update Simultaneous_P100_Spec_DAQ_Reading.py

--- Simultaneous_P100_Spec_DAQ_Reading.py
+++ Simultaneous_P100_Spec_DAQ_Reading.py
@@ -16,22 +16,15 @@ time_start =  time.time()
 
 # ########## A function for reading the spectrometer intensities ########### '''
 def Spec_Read_Process(No_Spec_Sample):
-    for Spec_Index_Process in range(No_Spec_Sample):
+    while Spec_Index[0] < (No_Spec_Sample -1):
         #Time_Label = time.time()
-        Current_Spec_Record[:], Spec_Time[Spec_Index_Process]  = Spec1.readIntensity(True, True)
+        Current_Spec_Record[:], Spec_Time[Spec_Index[0]]  = Spec1.readIntensity(True, True)
         #Spec_Time[Spec_Index_Process] = (time.time() )
         Spec_Is_Read.value = 1
         #Spec_Index_Process = Spec_Index_Process + 1
-        print "spectrometer Index is %i" % Spec_Index_Process
-        #print (Current_Spec_Record[0] - Time_Label)
-
-    '''
-    for I in range(No_Spec_Sample):
-        #Time_Label = time.time()
-        Current_Spec_Record[:] = Spec1.readIntensity(True, True)
-        Spec_Is_Read.value = 1
-        #print (Current_Spec_Record[0] - Time_Label)
-    '''
+        Spec_Index[0] = Spec_Index[0] + 1
+        print "spectrometer Index is %i" % Spec_Index[0]
+    Spec_Is_Done.value = 1
 
 
 # ######## A function for reading the DAQ analogue inpute on AINX ########
@@ -69,6 +62,8 @@ if __name__ == "__main__":
 
     Spec_Is_Read = Value('i', 0)
     Spec_Is_Read.value = 0
+    Spec_Is_Done = Value('i', 0)
+    Spec_Is_Done.value = 0
     DAQ_Is_Read = Value('i', 0)
     DAQ_Is_Read.value = 0
     Power_Is_Read = Value('i', 0)
@@ -76,17 +71,17 @@ if __name__ == "__main__":
     Timer_Is_Over = Value('i', 0)
     Timer_Is_Over.value = 0
 
-
-    No_DAC_Sample = 1000       # Number of samples for Photodiod per iteration of the laser exposer. Every sample takes ~0.6 ms.
-    No_Power_Sample = int(round(No_DAC_Sample/3))
-    No_Spec_Sample = int(round(No_DAC_Sample/(.4*Integration_Time)))
+    DurationOfReading = 3.12      # Duration of reading in seconds.
+    No_DAC_Sample =   int(round(DurationOfReading*1000/1.7))                # Number of samples for DAQ analogue to digital converter (AINx). Roughly DAQ can read AIN1 2 and 3 evry 1.5 ms and 2.4 ms for AIN0,
+    No_Power_Sample = int(round(DurationOfReading*1000/5.1))                # Number of samples for P100D Power meter to read. Roughly P100 can read the power every 2.7 ms.
+    No_Spec_Sample =  int(round(DurationOfReading*1000/(Integration_Time))) # Number of samples for spectrometer to read. It takes integration time can read the power every 2.7 ms.
 
     Current_Spec_Record = Array('d', np.zeros(shape=( len(Spec1.Handle.wavelengths()) ,1), dtype = float ))
     #Spec_Index = Array('i', np.zeros(shape=( 1 ,1), dtype = int ))
     Full_Spec_Records = np.zeros(shape=(len(Spec1.Handle.wavelengths()), No_Spec_Sample ), dtype = float )
     Spec_Time   = Array('d', np.zeros(shape=( No_Spec_Sample ,1), dtype = float ))
-    Spec_Index = 0
-    Spec_Index2 = Array('i', np.zeros(shape=( 1 ,1), dtype = int ))
+    #Spec_Index = 0
+    Spec_Index = Array('i', np.zeros(shape=( 1 ,1), dtype = int ))
 
     DAQ_Signal = Array('d', np.zeros(shape=( No_DAC_Sample ,1), dtype = float ))
     DAQ_Time   = Array('d', np.zeros(shape=( No_DAC_Sample ,1), dtype = float ))
@@ -108,22 +103,21 @@ if __name__ == "__main__":
     Pros_Spec.start()
 
 
-    while((Spec_Index < No_Spec_Sample)):
+    while((Spec_Is_Done.value == 0)):
         if  Spec_Is_Read.value == 1:
             Spec_Is_Read.value = 0
-            Full_Spec_Records[1:, Spec_Index] = Current_Spec_Record[1:]
-            #Full_Spec_Records[0, Spec_Index] = (Current_Spec_Record[0])*1000000000000
-            #Full_Spec_Records[0, Spec_Index] = time.time()
-            #print ('time %f' % Full_Spec_Records[0, Spec_Index])
-            #Spec_Time[Spec_Index] = Current_Spec_time
-            Spec_Index = Spec_Index + 1
+            Full_Spec_Records[:, np.int(Spec_Index[0])] = Current_Spec_Record[:]
+            #print 'Spec_Index: %i'  %np.int(Spec_Index[0])
             #print ('dsds' , Spec_Index)
     print('Spectrometer is done')
+    #Full_Spec_Records = Full_Spec_Records[:, 0:Spec_Index]
+    #Spec_Time2 = np.asarray(Spec_Time[0:Spec_Index])
+    #Spec_Time  = Spec_Time2
     while True:
         if ((DAQ_Is_Read.value == 1) & (Power_Is_Read.value == 1)):
             break
 
-
+    time.sleep(0.1)
     DAQ1.close()
     Spec1.close()
 
@@ -162,7 +156,7 @@ if __name__ == "__main__":
     plt.figure()
     plt.plot(DAQ_Time, (DAQ_Signal[0:DAQ_Index[0]]-np.mean(DAQ_Signal))/float( np.max(np.abs(DAQ_Signal))))
     plt.plot(Power_Time, (Power_Signal[0:Power_Index[0]]-np.mean(Power_Signal))/float( np.max(np.abs(Power_Signal))))
-    plt.title("Normalized Power and DAQ signals")
+    plt.title("Super imposed Power and DAQ signals (not the actual signals)")
     plt.xlabel("Unix time")
     plt.legend(['DAQ', 'P100'])
     plt.show()
@@ -190,10 +184,10 @@ if __name__ == "__main__":
     plt.ylabel("Time (s)")
 
     plt.subplot(1,3,3)
-    Spec_Latency = Spec_Time[0:Spec_Index]
+    Spec_Latency = Spec_Time[0:np.int(Spec_Index[0])]
     Spec_Latency[0] = 0
-    for I in range(1,Spec_Index):
-        Spec_Latency[I] = Spec_Time[I] - Spec_Time[I-1]
+    for I in range(1,Spec_Index[0]):
+        Spec_Latency[I] = np.float(Spec_Time[I] - Spec_Time[I-1])
     plt.plot(Spec_Latency)
     plt.ylabel("Time (s)")
     plt.title("Spectrometer integration durations")
